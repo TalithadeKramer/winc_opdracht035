3a08eaac9975eec5cf07ca30902ab3f038f3a16f
TalithadeKramer <76871148+TalithadeKramer@users.noreply.github.com>
2021-05-03 20:29:05 +0200

    Add files via upload

---
+++ Opdracht_035/script.js
@@ -0,0 +1,28 @@
+let woord = ["nice", "awesome", "tof"];
+console.log(woord.length);
+
+
+let newLength = woord.push('cool');
+console.log(woord);
+
+console.log(woord.length);
+
+let first = woord[0];
+console.log(first);
+
+let last = woord[woord.length - 1];
+console.log(last);
+
+
+
+const presidents = ["Trump", "Obama", "Bush", "Clinton"] 
+  
+  const impeachTrumpSlice = function(array) {
+  presidents.splice
+  }
+  const impeachTrumpSplice = function(array) {
+        (...)
+  }
+  
+  console.log(impeachTrumpSlice(presidents)); // ["Obama", "Bush", "Clinton"]
+  console.log(impeachTrumpSplice(presidents)); // ["Obama", "Bush", "Clinton"]
